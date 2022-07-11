c31de1c91e7f0a00d86c84df2a4785dd75d3629a
René Seib <70758072+reneseib@users.noreply.github.com>
2022-07-11 18:56:48 +0200

    finalized geotree/main.py to work

--- geotree/main.py
+++ geotree/main.py
@@ -2,19 +2,34 @@ import warnings
 
 warnings.simplefilter("ignore", UserWarning)
 
-
-import fiona
 import timeit
 from shapely.geometry import Polygon, MultiPolygon
 import numpy as np
 import pandas as pd
+import geopandas as gpd
 import os
 import sys
 import numba
 from numba import njit, prange, typeof, typed, types
-from numba.typed import Dict, List
-from itertools import chain
+from numba.typed import Dict
+from math import ceil
 
+# Custom Imports
+from util import (
+    load_geometries,
+    generate_leaves,
+    get_bbox,
+    get_centroid,
+    global_polygon_dissolve,
+    grow_tree,
+    get_leaf_to_load,
+    get_leaf_collection,
+    batch_centroids,
+    get_distance,
+    collect_distances,
+    calculate_shortest_distance,
+    status_bar,
+)
 
 """
 The general idea to improve the minimum distance calculation time is to first check if our point (the point we specified) is inside a 'tree leaf'. Tree leaves are the result of combining the vertices of the global BBOX and a weighted center point, the global centroid of our dataset.
@@ -25,102 +40,175 @@ If it is inside the 'tree leaf', we go to that leaf and check if our point is in
 
 We repeat this to the lowest level and only then calculate the distances, determine the minimum value from it and return it.
 """
+t0 = timeit.default_timer()
 
+#
+#
+#
+#
+#
 
-# Setup
+regio = "nordrhein_westfalen"
+
 t1 = timeit.default_timer()
-geo_file = "/common/ecap/prospector_data/results/stages/3-filtered_by_intersection_protected_area/bremen/gpkg/bremen-3-filtered_by_intersection_protected_area.gpkg"
 
-lsg_file = (
-    "/common/ecap/prospector_data/src_data/protected_areas/gpkg/nsg_gesamt_de.gpkg"
+primary_file = f"/common/ecap/prospector_data/results/stages/3-filtered_by_intersection_protected_area/{regio}/gpkg/{regio}-3-filtered_by_intersection_protected_area.feather"
+
+secondary_file = (
+    "/common/ecap/prospector_data/src_data/protected_areas/gpkg/lsg_gesamt_de.feather"
 )
 
+CRS = {
+    "thueringen": 25833,
+    "sachsen": 25833,
+    "sachsen_anhalt": 25833,
+    "berlin": 25833,
+    "brandenburg": 25833,
+    "baden_wuerttemberg": 25832,
+    "bayern": 25832,
+    "bremen": 25832,
+    "hamburg": 25832,
+    "hessen": 25832,
+    "mecklenburg_vorpommern": 25832,
+    "niedersachsen": 25832,
+    "nordrhein_westfalen": 25832,
+    "rheinland_pfalz": 25832,
+    "schleswig_holstein": 25832,
+    "saarland": 25832,
+}
 
-@njit
-def column_split(input, pos1, pos2):
-    x1 = pos1
-    x2 = pos2 + 1
-    cols = x2 - x1
-    res = List()
-    for i in range(len(input)):
-        row = input[i]
-        res.append(row[x1:x2])
-    return res
+primary_src_data = gpd.read_feather(primary_file)
 
+# Transform to EPSG:25832 if necessary
+primary_src_crs = CRS[regio]
 
-@njit
-def construct_true_bbox(bbox_arr):
-    minx, miny, maxx, maxy = bbox_arr
+if primary_src_crs != 25832:
+    primary_src_data = primary_src_data.set_crs(
+        primary_src_crs, allow_override=True
+    ).to_crs(25832)
 
-    p1 = [minx, miny]
-    p2 = [minx, maxy]
-    p3 = [maxx, maxy]
-    p4 = [maxx, miny]
+# Load data to typed dict
+PRIMARY_DATA = load_geometries(primary_src_data)
 
-    bbox = [p1, p2, p3, p4]
-
-    return bbox
-
-
-@njit
-def get_bbox(input):
-    xcol = column_split(input, 0, 0)
-    ycol = column_split(input, 1, 1)
-
-    minx = min(xcol)
-    miny = min(ycol)
-    maxx = max(xcol)
-    maxy = max(ycol)
-
-    return List([minx, miny, maxx, maxy])
+t1 = timeit.default_timer() - t0
+print(f"PRIMARY_DATA LOADED - {t1} sec.")
 
 
-@njit
-def get_centroid(input):
-    cx, cy = np.sum(input, axis=0)
-    cx = cx / len(input)
-    cy = cy / len(input)
-    result = List([cx, cy])
-    return result
+t0 = timeit.default_timer()
+
+secondary_src_data = gpd.read_feather(secondary_file)
+secondary_src_data = secondary_src_data.set_crs(25832, allow_override=True)
+SECONDARY_DATA = load_geometries(secondary_src_data)
+
+t1 = timeit.default_timer() - t0
+print(f"SECONDARY_DATA LOADED - {t1} sec.")
 
 
-@njit
-def get_distance(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
+t0 = timeit.default_timer()
+TREE = grow_tree(PRIMARY_DATA, SECONDARY_DATA)
+t1 = timeit.default_timer() - t0
+print(f"Grew the tree - {t1} sec.")
 
-    a = abs(x1 - x2)
-    b = abs(y1 - y2)
-    c = np.sqrt(a ** 2 + b ** 2)
-    return c
+tcalcstart = timeit.default_timer()
+SD_centroids = np.array(batch_centroids(SECONDARY_DATA)).astype(np.float64)
 
+for i in range(len(PRIMARY_DATA.keys())):
+    keys = list(PRIMARY_DATA.keys())
+    key = keys[i]
+    PriDat_polygon = PRIMARY_DATA[key]
+    PriDat_centroid = get_centroid(PriDat_polygon)
+    #
+    # print("LEAVES")
+    # print(TREE["primary"]["leaves"])
+    # print("")
+    # print("CURRENT POLY CENTROID")
+    # print(PriDat_centroid)
+    # print("MIN/MAX BBOX")
+    # print([280374.9680000003, 5235854.618000001, 919678.7967999995, 6091118.6339])
 
-@njit
-def within_bbox(bounds, point):
-    minx, miny, maxx, maxy = bounds
+    leaf_to_load = get_leaf_to_load(TREE["primary"]["leaves"], PriDat_centroid)
+    if leaf_to_load is not None:
+        if len(TREE["primary"]["collections"][leaf_to_load]) == 0:
 
-    px, py = point
+            leaf_collection = get_leaf_collection(
+                TREE["primary"]["leaves"], leaf_to_load, SECONDARY_DATA
+            )
 
-    if px >= minx and px <= maxx and py >= miny and py <= maxy:
-        return 1
+            TREE["primary"]["collections"][leaf_to_load] = leaf_collection
+
     else:
-        return 0
+        print("LEAVES")
+        print(TREE["primary"]["leaves"])
+        print("")
+        print("CURRENT POLYGON")
+        print(PriDat_polygon)
+        print("")
+        print("POLY CENTROID")
+        print(PriDat_centroid)
+        sys.exit()
+        continue
+
+    # # Then collect the leaves for the child; here PD
+    # if len(TREE["secondary"]["collections"][leaf_to_load]) == 0:
+    #     TREE["secondary"]["collections"][leaf_to_load] = get_leaf_collection(
+    #         TREE["secondary"]["leaves"], leaf_to_load, PRIMARY_DATA
+    #     )
+
+    # Once we have the collections, we check the distances
+    # to the centroids of all polygons.
+    # 1. Get all polygon ids
+    polygon_ids = TREE["primary"]["collections"][leaf_to_load]
+
+    # Create empty 'result 2D array' in which we store
+    # the values as [ID, DISTANCE]
+    id_distances_arr = np.empty((len(polygon_ids), 2)).astype(np.float64)
+    idd_sample_arr = np.array([1, 2.345])
+
+    # Prepare an array with all centroids for all polygons
+    # within that leaf
+    id_distances_arr = collect_distances(
+        id_distances_arr, idd_sample_arr, polygon_ids, SD_centroids, PriDat_centroid
+    )
+
+    # 5. Sort the id_distances_arr by ascending distance (column [1])
+    sorted_id_distances_arr = id_distances_arr[id_distances_arr[:, 1].argsort()]
+
+    # 6. Return the n-th shortest distances
+    n = ceil(len(id_distances_arr) * 0.00625)
+    if n < 11:
+        if len(id_distances_arr) > 7:
+            n = 7
+        else:
+            n = ceil(len(id_distances_arr) / 2)
+
+    if n > 0:
+        nth_sorted_id_distances = sorted_id_distances_arr[:n]
+
+        # Load the real polygon points from the IDs of the shortlist
+        closest_sd_ids = np.array([x[0] for x in nth_sorted_id_distances]).astype(
+            np.float64
+        )
+
+        rd_polygon_ids_on_this_leaf = np.array(
+            TREE["primary"]["collections"][leaf_to_load]
+        ).astype(np.float64)
+
+        t0 = timeit.default_timer()
+        shortest_distance = calculate_shortest_distance(
+            PriDat_polygon, closest_sd_ids, SECONDARY_DATA
+        )
+        t1 = timeit.default_timer() - t0
+
+        show = status_bar(i, len(PRIMARY_DATA))
+
+        # print(
+        #     f"Min. Distance: {round(shortest_distance / 1000,2)} km  --  Calculation Time: {t1}. sec"
+        # )
+    else:
+        print(f"\nSkipped: PRIMARY_DATA - key:{key}\n")
 
 
-@njit
-def overlaps(bounds, bbox):
-    # bboxes always has a length of 4 so it's safe to go directly with int(4)
-    bbox_len = 4
-
-    _results = np.zeros(bbox_len, dtype=np.int16)
-
-    for i in range(bbox_len):
-        point = bbox[i]
-        _results[i] = within_bbox(bounds, point)
-
-    bin_count = np.bincount(_results)
-    if len(bin_count) == 2:
-        if np.bincount(_results)[1] >= 1:
-            return True
-
-    return False
+t2 = timeit.default_timer() - tcalcstart
+print()
+print("-" * 10)
+print(f"For {len(PRIMARY_DATA) * len(SECONDARY_DATA)} calculations: {round(t2,2)} sec.")
--- get.py
+++ get.py
@@ -1,118 +1,39 @@
-import sys
-import os
 import numpy as np
-import numba
-from numba import jit, njit, typeof
-from numba.core import types
-from numba.typed import Dict, List
-import timeit
-import fiona
-from itertools import chain
+import polars as pl
 
-nb_list = List.empty_list(item_type=types.float64)
-nb_nested_list = List(
-    [
-        nb_list,
-        nb_list,
-        nb_list,
-        nb_list,
-    ]
+bla = pl.read_csv(
+    "/Users/shellsquid/dev/green/geodata/FINAL/2-after_adding_centroid/csv/bayern.csv",
+    delimiter=";",
+    decimal=",",
 )
 
-
-collections_list_dict = Dict.empty(key_type=types.int64, value_type=typeof(nb_list))
-
-
-TREE_collections = Dict.empty(
-    key_type=types.unicode_type, value_type=typeof(collections_list_dict)
-)
-
-
-for i in range(4):
-    collections_list_dict[i] = nb_list
-
-
-TREE_collections["parent"] = collections_list_dict
-TREE_collections["child"] = collections_list_dict
-
-TREE_leaves = nb_nested_list
-
-print(TREE_leaves)
-print("")
-print(TREE_collections)
-
-
+print(len(bla))
+# bla["area_ha"] = bla["area_ha"].apply(lambda x: float(x.replace(",", ".")))
 #
-# t0 = timeit.default_timer()
+# bla = bla.filter(pl.col("area_ha") > 25)
 #
-# geo_file = "/common/ecap/prospector_data/results/stages/3-filtered_by_intersection_protected_area/baden_wuerttemberg/gpkg/baden_wuerttemberg-3-filtered_by_intersection_protected_area.gpkg"
+# np.random.
 #
-#
-# def create_typed_dicts():
-#     nested_array = np.array([[]]).astype(np.float64)
-#
-#     int_arr_dict = Dict.empty(key_type=types.int64, value_type=typeof(nested_array))
-#
-#     str_arr_dict = Dict.empty(
-#         key_type=types.unicode_type,
-#         value_type=typeof(int_arr_dict),
+# bla = bla["area_ha"].apply(
+#     lambda x: np.sqrt(
+#         x * (np.array([0.28734782487, 2.3478874, 813.723476877659]) ** 1.1)
 #     )
-#
-#     data_dict = Dict.empty(
-#         key_type=types.int64,
-#         value_type=typeof(str_arr_dict),
-#     )
-#     return int_arr_dict, str_arr_dict, data_dict
+# )
+# print(bla)
+
+
 #
 #
-# int_arr_dict, str_arr_dict, data_dict = create_typed_dicts()
+# for file in os.listdir(base_dir):
+#     if file.endswith(".gpkg"):
 #
+#         file_path = os.path.join(base_dir, file)
 #
-# t1 = timeit.default_timer() - t0
-#
-# print("Dict creation took:", t1)
-#
-# t2 = timeit.default_timer()
-# with fiona.open(geo_file) as geo:
-#     geo = dict(geo)
-#
-#     data_keys = list(geo.keys())
-#     geometries = {}
-#     for k in data_keys:
-#         coords = geo[k]["geometry"]["coordinates"]
-#         geometries[k] = coords
-#
-#     for key in data_keys:
-#         geom = geometries[key]
-#         geom = list(chain(*geom))
-#
-#         # Copy the typed dictionaries for each geometry
-#         int_arr_dict = int_arr_dict.copy()
-#         str_arr_dict = str_arr_dict.copy()
-#
-#         if type(geom[0]) != list:
-#             geom = np.array([np.array(x).astype(np.float64) for x in geom]).astype(
-#                 np.float64
-#             )
-#             int_arr_dict[0] = geom.reshape(len(geom), 2)
-#
-#         else:
-#             geom = [np.array(x).astype(np.float64) for x in geom]
-#             for i in range(len(geom)):
-#                 g = geom[i]
-#                 g = g.reshape(len(g), 2)
-#
-#                 int_arr_dict[i] = g
-#
-#         str_arr_dict["geometry"] = int_arr_dict
-#
-#         data_dict[key] = str_arr_dict
-#
-#
-# t3 = timeit.default_timer() - t2
-# t4 = timeit.default_timer()
-# print("Assigning values took:", t3)
-#
-# print("In total:", t4 - t0)
-#
-# # print(data_dict)
+#         target_fname = file.replace(".gpkg", ".feather")
+#         out_path = os.path.join(base_dir, target_fname)
+#         if not os.path.exists(out_path):
+#             data = gpd.read_file(file_path)
+#             data["geomarr"] = data["geometry"].apply(lambda x: convert(x))
+#             data.to_feather(out_path)
+#             print(f">> {target_fname} created")
+# print("")
