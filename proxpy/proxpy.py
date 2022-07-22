05bc09f91b6a2ea0d12e542a9fae8def58f67580
René Seib <70758072+reneseib@users.noreply.github.com>
2022-07-22 10:15:19 +0200

    Added automatic ip list update function

--- proxpy/proxpy.py
+++ proxpy/proxpy.py
@@ -27,13 +27,30 @@ class ProxyRequest(object):
 
         return ip_list
 
+    def update_ip_list(self):
+        while True:
+            if time.time() - self.last_update > 1800:
+                # Update IP list
+                self.ip_list = ProxyRequest.generate_ip_list()
+                # Update last_update to now
+                self.last_update = time.time()
+
     def __init__(self):
         self.ip_list = ProxyRequest.generate_ip_list()
-        self.setup_time = time.time()
-        # TODO: Add function to refresh list after 60min
+        self.lastidx = []
+        self.range = list(range(0, len(self.ip_list)))
+        self.last_update = time.time()
+        self.update = self.update_ip_list()
+
+    def get_idx(self):
+        if len(self.range) == 0:
+            self.range = list(range(0, len(self.ip_list)))
+        else:
+            idx = self.range.pop(0)
+            return idx
 
     def get(self, url, headers=None):
-        randint = random.randint(0, len(self.ip_list) - 1)
-        proxies = {"socks": f"socks5://{self.ip_list[randint]}"}
+        idx = get_idx()
+        proxies = {"socks": f"socks5://{self.ip_list[idx]}"}
         response = requests.get(url, headers=headers, proxies=proxies)
         return response
