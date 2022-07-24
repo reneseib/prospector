771e52bfd2bed9ceed9ec4694699681aa3388dba
René Seib <70758072+reneseib@users.noreply.github.com>
2022-07-24 18:57:03 +0200

    Further built stage 5

--- prospector/stages/stage_5/stage_5.py
+++ prospector/stages/stage_5/stage_5.py
@@ -70,16 +70,18 @@ def f_stage_5(regio, stage="5-added_nearest_substation"):
 
         # Load file from previous stage to a GDF
         gdf = util.load_prev_stage_to_gdf(regio, stage)
+        print("LEN GDF")
+        print(len(gdf))
         print(f"{regio}'s previous stage loaded - starting to iterate over GDF")
 
         targets = {
             "solar": f"{regio}-solar_power",
             "wind": f"{regio}-wind_power",
-            "hydro": f"{regio}hydro_power",
-            "residential": f"{regio}-residential_area",
-            "commercial": f"{regio}-commercial_area",
-            "industrial": f"{regio}-industrial_area",
-            "raildways": f"{regio}-railways",
+            "hydro": f"{regio}-hydro_power",
+            "residential": f"{regio}-residential_areas",
+            "commercial": f"{regio}-commercial_areas",
+            "industrial": f"{regio}-industrial_areas",
+            "railways": f"{regio}-railways",
             "roads": f"{regio}-roads",
         }
 
@@ -91,11 +93,23 @@ def f_stage_5(regio, stage="5-added_nearest_substation"):
 
             if len(gdf) > 0 and len(target_gdf) > 0:
                 # Set to their original CRS, project always to epsg:25832 in case we run against something on national level which is always in 25832
+
+                for col in gdf.columns:
+                    if (
+                        "_left" in col
+                        or "_right" in col
+                        or "_x" in col
+                        or "_y" in col
+                        or col.startswith("np_")
+                    ):
+                        gdf = gdf.drop(columns=[col])
+
                 if config["epsg"][regio] != 25832:
                     print("need to convert those gdfs")
                     gdf = gdf.set_crs(
                         config["epsg"][regio], allow_override=True
                     ).to_crs(25832)
+
                     target_gdf = target_gdf.set_crs(
                         config["epsg"][regio], allow_override=True
                     ).to_crs(25832)
@@ -104,25 +118,135 @@ def f_stage_5(regio, stage="5-added_nearest_substation"):
 
                 dist_gdf = gpd.sjoin_nearest(gdf, target_gdf, distance_col="distance")
 
-                print(dist_gdf["distance"])
+                # Get back the normal id column
+                dist_gdf["id"] = dist_gdf["id_left"]
+                dist_gdf = dist_gdf.drop(columns=["id_left"])
 
-            #     # Save processing results to disk
-            #     stage_successfully_saved = util.save_current_stage_to_file(
-            #         gdf, regio, stage
-            #     )
-            #     if stage_successfully_saved:
-            #         cols = []
-            #         for col in gdf.columns:
-            #             if "distance" in col:
-            #                 cols.append(col)
-            #         print(gdf[cols].head(20))
-            #         print("Stage successfully saved to file")
-            #         return True
-            #     else:
-            #         return False
-            #
-            #     print("for all PA:", timeit.default_timer() - t)
-            #
-            # else:
-            #     # File already exists, return False
-            #     return False
+                # Convert to pandas dataframe to make the merge
+                # which isn't possible in geopandas
+                pd_dist = pd.DataFrame(dist_gdf)
+                pd_gdf = pd.DataFrame(gdf)
+
+                # pd_dist["id"] = pd_dist["id_left"]
+                # pd_dist = pd_dist.drop(columns=["id_left", "id_right"])
+
+                try:
+                    dist_target_cols = list(pd_dist.columns.difference(pd_gdf.columns))
+
+                    dist_target_cols.append("id")
+
+                    for col in dist_target_cols:
+                        if "_left" in col or "_right" in col:
+                            dist_target_cols.remove(col)
+
+                    mrgd_gdf = pd.merge(
+                        pd_gdf,
+                        pd_dist[dist_target_cols],
+                        how="outer",
+                        on="id",
+                    )
+
+                except:
+                    print("pd_gdf:", pd_gdf.columns)
+                    print("pd_dist:", pd_dist.columns)
+                    raise
+
+                # Back to GDF & rename distance column
+                gdf = gpd.GeoDataFrame(mrgd_gdf)
+                gdf[f"nearest_{target}"] = gdf["distance"]
+                gdf = gdf.drop(columns=["distance"])
+
+                print(gdf.columns)
+                print(gdf[["id", f"nearest_{target}"]])
+                gdf = gdf.drop_duplicates(
+                    subset=["id", f"nearest_{target}", "geometry"]
+                )
+                print(gdf[["id", f"nearest_{target}"]])
+
+                for col in gdf.columns:
+                    if (
+                        "_left" in col
+                        or "_right" in col
+                        or "_x" in col
+                        or "_y" in col
+                        or col.startswith("np_")
+                    ):
+                        gdf = gdf.drop(columns=[col])
+
+    drop_cols = [
+        "disused",
+        "osm_type",
+        "railway",
+        "tags",
+        "timestamp",
+        "version",
+        "nearest_railways",
+        "access",
+        "area",
+        "bicycle",
+        "bridge",
+        "cycleway",
+        "foot",
+        "highway",
+        "int_ref",
+        "junction",
+        "lanes",
+        "lit",
+        "maxspeed",
+        "motorcar",
+        "motorroad",
+        "name",
+        "oneway",
+        "overtaking",
+        "psv",
+        "ref",
+        "segregated",
+        "sidewalk",
+        "smoothness",
+        "surface",
+        "tunnel",
+        "turn",
+        "width",
+        "farmland",
+        "meadow",
+        "changeset",
+        "power",
+        "construction",
+        "landuse",
+        "residential",
+        "commercial",
+        "depot",
+        "industrial",
+        "busway",
+        "footway",
+        "motor_vehicle",
+        "service",
+        "tracktype",
+    ]
+    cols_to_drop = []
+    for dcol in drop_cols:
+        if dcol in gdf.columns:
+            cols_to_drop.append(dcol)
+    gdf = gdf.drop(columns=cols_to_drop)
+
+    print(gdf.columns)
+    #     # Save processing results to disk
+    #     stage_successfully_saved = util.save_current_stage_to_file(
+    #         gdf, regio, stage
+    #     )
+    #     if stage_successfully_saved:
+    #         cols = []
+    #         for col in gdf.columns:
+    #             if "distance" in col:
+    #                 cols.append(col)
+    #         print(gdf[cols].head(20))
+    #         print("Stage successfully saved to file")
+    #         return True
+    #     else:
+    #         return False
+    #
+    #     print("for all PA:", timeit.default_timer() - t)
+    #
+    # else:
+    #     # File already exists, return False
+    #     return False
