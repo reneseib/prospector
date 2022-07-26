c91c5f98697e903730bd4c66f584251485e47711
René Seib <70758072+reneseib@users.noreply.github.com>
2022-07-26 22:39:47 +0200

    Finished stage 7

--- prospector/stages/stage_7/stage_7.py
+++ prospector/stages/stage_7/stage_7.py
@@ -6,6 +6,7 @@ sys.path.append(parent_dir)
 # Stage 0 Imports
 from pyrosm import OSM, get_data
 import geopandas as gpd
+import pandas as pd
 from shapely.geometry import Point, Polygon, MultiPolygon, mapping
 from shapely import wkt
 from pyrosm import OSM, get_data
@@ -37,4 +38,77 @@ def f_stage_7(regio, stage="7-added_nearest_agrargen"):
     Adds the name, details and distance of the area's
     nearest 'Agrargenossenschaft'
     """
-    return None
+    output_file_gpkg = os.path.join(
+        results_dir,
+        "stages",
+        stage,
+        regio,
+        "gpkg",
+        f"{regio}-{stage}.gpkg",
+    )
+    # Before starting the whole process, check if the output file already exists
+    print(f"{regio}: Starting stage 7 now")
+
+    agrar_file = "/common/ecap/prospector_data/src_data/entities/agrargenossenschaften/agrar-genossenschaften.gpkg"
+
+    data = gpd.read_file(agrar_file)
+
+    agro_gdf = gpd.GeoDataFrame(data)
+
+    output_file_gpkg = os.path.join(
+        results_dir,
+        "stages",
+        stage,
+        regio,
+        "gpkg",
+        f"{regio}-{stage}.gpkg",
+    )
+
+    # Load previous stage data
+    gdf = util.load_prev_stage_to_gdf(regio, stage)
+
+    agro_dist = gpd.sjoin_nearest(gdf, agro_gdf, distance_col="distance", how="inner")
+
+    gdf["nearest_agrargen"] = None
+    gdf["nearest_agrargen_info"] = None
+
+    agro_dist = agro_dist.drop_duplicates(subset=["area_m2"])
+
+    if len(agro_dist) == len(gdf):
+        for i in range(len(agro_dist)):
+            distance = agro_dist.loc[i, "distance"]
+            agro_row = agro_dist.loc[
+                i,
+                [
+                    "firma",
+                    "adresse",
+                    "plz",
+                    "stadt",
+                    "tel",
+                    "ackerland",
+                    "hopfen",
+                    "gruenland",
+                    "wald",
+                    "gemuese",
+                    "energie",
+                    "obst",
+                    "blumen",
+                    "gewuerze",
+                    "wein",
+                ],
+            ]
+            gdf.at[i, "nearest_agrargen_info"] = agro_row.to_json()
+            gdf.at[i, "nearest_agrargen"] = distance
+
+        print(f"{regio}: saving now")
+        stage_successfully_saved = util.save_current_stage_to_file(gdf, regio, stage)
+        if stage_successfully_saved == True:
+            return True
+        else:
+            return False
+    else:
+        print("=========================================")
+        print(f"ERROR: {regio} GDFs have not the same length!!")
+        print("=========================================")
+
+        return False
