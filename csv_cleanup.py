d5516a87adcb45016f6d741fbf5161f524c3ba3a
René Seib <70758072+reneseib@users.noreply.github.com>
2022-07-28 10:44:20 +0200

    Loop to clean up and reformat all csv files to German settings

---
+++ csv_cleanup.py
@@ -0,0 +1,40 @@
+import os, sys
+
+regios = [
+    "saarland",
+    "berlin",
+    "bremen",
+    "hamburg",
+    "hessen",
+    "rheinland_pfalz",
+    "sachsen",
+    "schleswig_holstein",
+    "brandenburg",
+    "bayern",
+    "nordrhein_westfalen",
+    "thueringen",
+    "niedersachsen",
+    "sachsen_anhalt",
+    "baden_wuerttemberg",
+    "mecklenburg_vorpommern",
+]
+
+
+for regio in regios:
+
+    file = f"/common/ecap/prospector_data/results/final/{regio}/csv/{regio}_final2.csv"
+
+    cleaned = None
+
+    with open(file, "r") as f:
+        text = f.read()
+
+        text = text.replace(",", ";").replace(".", ",")
+        cleaned = text
+
+    clean_file = (
+        f"/common/ecap/prospector_data/results/final/{regio}/csv/{regio}_final.csv"
+    )
+
+    with open(clean_file, "w") as file:
+        file.write(cleaned)
